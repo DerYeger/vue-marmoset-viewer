f2362447fdab0d0f7304950484ddaba7196ee672
Jan Müller <janmueller3698@gmail.com>
2021-07-15 14:07:25 +0200

    feat: export loadMarmoset

--- marmoset-viewer.d.ts
+++ marmoset-viewer.d.ts
@@ -2,3 +2,5 @@ import Vue, { PluginFunction, VueConstructor } from 'vue'
 
 declare const MarmosetViewer: VueConstructor<Vue> & { install: PluginFunction<any> }
 export default MarmosetViewer
+
+export declare function loadMarmoset(): Promise<void>
